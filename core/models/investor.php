19dc5d324cf36da8b40807e2c589e6796c822331
Alexey Rytikov <user.raa@gmail.com>
2017-11-23 14:44:40 +0300

    Investor: divide queries in db_init

--- core/models/investor.php
+++ core/models/investor.php
@@ -57,6 +57,8 @@ class Investor
                 `phone` varchar(254) NOT NULL,
                 PRIMARY KEY (`id`)
             );
+        ");
+        DB::query("
             CREATE TABLE IF NOT EXISTS `investors_coins` (
                 `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
                 `coin` varchar(32) NOT NULL,
