c70f06b127fc392f229e99928718fd9c394b8449
Alexey Rytikov <user.raa@gmail.com>
2017-11-28 13:20:32 +0300

    remove all id from html elements inside Wallet_view::newContribution(). JS now called only one time and Wallet_view::newContribution() may used multiple times on one page.

--- core/views/wallet_view.php
+++ core/views/wallet_view.php
@@ -9,18 +9,87 @@ use core\models\Wallet;
 
 class Wallet_view
 {
-    static private $already_myContribution = false;
+    static private $calledOnce_newContribution = false;
 
     static public function newContribution()
     {
-        if (self::$already_myContribution) {
-            return '<a href="#my-contribution-section">My Contribution</a>';
-        }
-        self::$already_myContribution = true;
         ob_start();
+
+        if (!self::$calledOnce_newContribution) {
+            self::$calledOnce_newContribution = true;
+            ?>
+            <script>
+                (function ($) {
+                    <?php
+                    $wallets = [];
+                    foreach (Coin::COINS as $coin) {
+                        $wallet = Wallet::getByInvestoridCoin(Application::$authorizedInvestor->id, $coin);
+                        $address = null;
+                        if ($wallet) {
+                            $address = @$wallet->address;
+                        }
+                        $wallets[$coin] = $address;
+                    }
+                    ?>
+                    var investorWalletAddresses = <?= json_encode($wallets, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?>;
+                    $(document).ready(function () {
+                        <?php
+                        $coinsRates = [];
+                        foreach (array_merge(Coin::COINS, [Coin::TOKEN]) as $coin) {
+                            $coinsRates[$coin] = Coin::getRate($coin);
+                        }
+                        ?>
+                        var coinsRate = <?= json_encode($coinsRates, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?>;
+                        var minimalTokensForNotToBeDonation = <?= json_encode(Deposit::minimalTokensForNotToBeDonation()) ?>;
+                        var onAmountChange = function (input) {
+                            var coin = $(input).parent().data('coin');
+                            var section = $(input).parents('.wallet_view-new_contribution-section');
+                            var text = $(input).val();
+                            if (text.length === 0) {
+                                return false;
+                            }
+                            var val = parseFloat(text);
+                            if ('' + val !== text) {
+                                $(input).val(val);
+                                return;
+                            } else if (val < 0) {
+                                $(input).val(-val);
+                                return;
+                            }
+                            val = val.toFixed(8);
+                            section.find('.wallet_view-new_contribution-selected_amount').text(val);
+
+                            var usd = coinsRate[coin] * val;
+                            var tokens = (usd / coinsRate['<?= Coin::TOKEN ?>']).toFixed(8);
+
+                            section.find('.wallet_view-new_contribution-calculated_selected_to_tokens').text(tokens);
+                            section.find('.wallet_view-new_contribution-calculated_as_donation').toggle(tokens < minimalTokensForNotToBeDonation);
+                        };
+                        $(document).on('keyup change', '.wallet_view-new_contribution-input_amount', function (event) {
+                            onAmountChange(event.target);
+                        });
+                        $(document).on('change', '.wallet_view-new_contribution-select_coins', function (event) {
+                            var coin = $(event.target).val();
+                            $('.wallet_view-new_contribution-div_amount').hide();
+                            $('.wallet_view-new_contribution-div_amount[data-coin=' + coin + ']').show();
+                            var section = $(event.target).parents('.wallet_view-new_contribution-section');
+                            var input = section.find('.wallet_view-new_contribution-div_amount[data-coin=' + coin + '] > input');
+                            onAmountChange(input);
+                            var walletAddrText = 'Wallet registration in progress';
+                            if (investorWalletAddresses[coin]) {
+                                walletAddrText = investorWalletAddresses[coin];
+                            }
+                            section.find('.wallet_view-new_contribution-selected_currency').text(coin);
+                            section.find('.wallet_view-new_contribution-selected_wallet_addr').text(walletAddrText);
+                        });
+                    });
+                })(jQuery);
+            </script>
+            <?php
+        }
         ?>
 
-        <section id="my-contribution-section" class="my-contribution">
+        <section class="my-contribution wallet_view-new_contribution-section">
             <div class="row">
                 <h3>My contribution</h3>
             </div>
@@ -32,103 +101,45 @@ class Wallet_view
                     <div class="row">
                         <form>
                             <div class="input-field col s6 m6">
-                                <select id="select-coins">
+                                <select class="wallet_view-new_contribution-select_coins">
                                     <?php foreach (Coin::COINS as $coin) { ?>
                                         <option value="<?= $coin ?>"><?= $coin ?></option>
                                     <?php } ?>
                                 </select>
-                                <label for="select-coins">select currency</label>
+                                <label>select currency</label>
                             </div>
                             <?php foreach (Coin::COINS as $coin) { ?>
-                                <div style="display: none;" id="div-amount-<?= $coin ?>" class="div-amount-coins input-field col s6 m6">
-                                    <input class="input-amount" id="input-amount-<?= $coin ?>" type="number"
-                                           value="1" min="0" max="9999999" step="0.000000001">
-                                    <label for="input-amount-<?= $coin ?>">select amount</label>
+                                <div style="display: none;" data-coin="<?= $coin ?>" class="wallet_view-new_contribution-div_amount input-field col s6 m6">
+                                    <input type="number"
+                                           class="wallet_view-new_contribution-input_amount"
+                                           value="1" min="0" max="9999999" step="0.00000001">
+                                    <label>select amount</label>
                                 </div>
                             <?php } ?>
                         </form>
-                        <script>
-                            (function ($) {
-                                <?php
-                                $wallets = [];
-                                foreach (Coin::COINS as $coin) {
-                                    $wallet = Wallet::getByInvestoridCoin(Application::$authorizedInvestor->id, $coin);
-                                    $address = null;
-                                    if ($wallet) {
-                                        $address = @$wallet->address;
-                                    }
-                                    $wallets[$coin] = $address;
-                                }
-                                ?>
-                                var investorWalletAddresses = <?= json_encode($wallets, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?>;
-                                $(document).ready(function () {
-                                    <?php
-                                    $coinsRates = [];
-                                    foreach (array_merge(Coin::COINS, [Coin::TOKEN]) as $coin) {
-                                        $coinsRates[$coin] = Coin::getRate($coin);
-                                    }
-                                    ?>
-                                    var coinsRate = <?= json_encode($coinsRates, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE) ?>;
-                                    var minimalTokensForNotToBeDonation = <?= json_encode(Deposit::minimalTokensForNotToBeDonation()) ?>;
-                                    var currentCoin = '';
-                                    var onAmountChange = function (input) {
-                                        var text = $(input).val();
-                                        if (text.length === 0) {
-                                            return false;
-                                        }
-                                        var val = parseFloat(text);
-                                        if ('' + val !== text) {
-                                            $(input).val(val);
-                                            return;
-                                        } else if (val < 0) {
-                                            $(input).val(-val);
-                                            return;
-                                        }
-                                        val = val.toFixed(8);
-                                        $('#selected_amount').text(val);
-                                        var usd = coinsRate[currentCoin] * val;
-                                        var tokens = usd / coinsRate['<?= Coin::TOKEN ?>'];
-                                        tokens = tokens.toFixed(8);
-                                        $('#calculated_selected_to_cpt').text(tokens);
-                                        $('#calculated_cpt_as_donation').toggle(tokens < minimalTokensForNotToBeDonation);
-                                    };
-                                    $('.input-amount').unbind('keyup change').bind('keyup change', function (e) {
-                                        onAmountChange(e.target);
-                                    });
-                                    $('#select-coins').on('change', function () {
-                                        var coin = $(this).val();
-                                        currentCoin = coin;
-                                        $('.div-amount-coins').hide();
-                                        $('#div-amount-' + coin).show();
-                                        $('#selected_currency').text(coin);
-                                        onAmountChange($('#input-amount-' + coin)[0]);
-                                        var walletAddrText = 'Wallet registration in progress';
-                                        if (investorWalletAddresses[coin]) {
-                                            walletAddrText = investorWalletAddresses[coin];
-                                        }
-                                        $('#selected_wallet_addr').html(walletAddrText);
-                                    }).change();
-                                });
-                            })(jQuery);
-                        </script>
                     </div>
                 </div>
             </div>
             <div class="row">
                 <p>
                     Copy address below to send
-                    <span id="selected_amount"></span>
-                    <span id="selected_currency"></span>
+                    <span class="wallet_view-new_contribution-selected_amount"></span>
+                    <span class="wallet_view-new_contribution-selected_currency"></span>
                 </p>
-                <h5 id="selected_wallet_addr"></h5>
+                <h5 class="wallet_view-new_contribution-selected_wallet_addr"></h5>
                 <p>
                     You will get:
-                    <span id="calculated_selected_to_cpt"></span>
-                    CPT
-                    <span id="calculated_cpt_as_donation" style="display: none;">(will be received as donation)</span>
+                    <span class="wallet_view-new_contribution-calculated_selected_to_tokens"></span>
+                    <?= Coin::TOKEN ?>
+                    <span class="wallet_view-new_contribution-calculated_as_donation" style="display: none;">(will be received as donation)</span>
                 </p>
             </div>
         </section>
+        <script>
+            $(document).ready(function () {
+                $('.wallet_view-new_contribution-select_coins').change();
+            });
+        </script>
 
         <?php
         return ob_get_clean();
