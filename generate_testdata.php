0ba4d7336ca5fa7672f81bc482af4ee26fb400bc
Alexey Rytikov <user.raa@gmail.com>
2017-12-01 19:34:10 +0300

    add wallets registration and receiving deposits inside test data

--- generate_testdata.php
+++ generate_testdata.php
@@ -4,7 +4,9 @@ require __DIR__ . '/loader.php';
 
 use core\controllers\Investor_controller;
 use core\engine\Application;
+use core\models\Deposit;
 use core\models\Investor;
+use core\models\Wallet;
 
 Application::init();
 
@@ -18,12 +20,20 @@ function randomPassword()
     return dechex(rand());
 }
 
-function randomEthAddr()
+function randomAddr()
 {
     return '0x' . sha1(rand());
 }
 
+$testCoin = 'DOGE';
+
 $newInvestors_id = [0];
 for ($i = 0; $i < 5000; $i++) {
-    $newInvestors_id[] = Investor::registerUser(randomEmail(), randomEthAddr(), $newInvestors_id[array_rand($newInvestors_id)], Investor_controller::hashPassword('qwerty1'));
+    $newInvestor_id = Investor::registerUser(randomEmail(), randomAddr(), $newInvestors_id[array_rand($newInvestors_id)], Investor_controller::hashPassword('qwerty1'));
+    $newInvestors_id[] = $newInvestor_id;
+    Wallet::registerWallet($newInvestor_id, $testCoin, randomAddr());
+    $amount = [1, rand(1, 20)][rand(0, 1)];
+    for ($j = 0; $j < rand(0, 5); ++$j) {
+        Deposit::receiveDeposit($amount, $testCoin, randomAddr(), 0, $newInvestor_id);
+    }
 }
