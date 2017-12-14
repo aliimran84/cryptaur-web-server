5feea7d21866f3620b930a121fbc9293f223bbba
Alexey Rytikov <user.raa@gmail.com>
2017-12-14 17:11:54 +0300

    PaymentServer_controller::handleNotify add isset keyid check; add case for NOTIFY_REASON_ALARM

--- core/controllers/paymentserver_controller.php
+++ core/controllers/paymentserver_controller.php
@@ -19,6 +19,7 @@ class PaymentServer_controller
 
     const NOTIFY_REASON_ADDRESS = 'address';
     const NOTIFY_REASON_DEPOSIT = 'deposit';
+    const NOTIFY_REASON_ALARM = 'alarm';
     const HMAC_HEADER = 'HMAC-Signature';
 
     static public function init()
@@ -79,27 +80,30 @@ class PaymentServer_controller
      */
     static public function handleNotify()
     {
-        $headers = getallheaders();
-        $receivedHmacHash = @$headers[self::HMAC_HEADER];
         $rawMessage = @file_get_contents('php://input');
         $message = @json_decode($rawMessage, true);
         if (!$message) {
             return -1;
         }
-        $calculatedHmac = self::messageHmacHash($message['keyid'], $rawMessage);
-        if ($receivedHmacHash !== $calculatedHmac) {
+        if (!isset($message['keyid'])) {
             return -2;
+        }
+        $headers = getallheaders();
+        $receivedHmacHash = @$headers[self::HMAC_HEADER];
+        $calculatedHmac = self::messageHmacHash(@$message['keyid'], $rawMessage);
+        if ($receivedHmacHash !== $calculatedHmac) {
+            return -3;
         }
 
         if (!PaymentServer::checkUpdateNonce($message['keyid'], $message['nonce'])) {
-            return -3;
+            return -4;
         }
 
         if ($message['reason'] === self::NOTIFY_REASON_ADDRESS) {
             //"{"address": "0xf410e1a3b6a42511d2113911111181116511711d", "coin": "eth", "keyid": "22c336448554e86b", "nonce": 1511527643485, "reason": "address", "userid": 1}"
             $coin = strtoupper($message['coin']);
             if (!Coin::issetCoin($coin)) {
-                return -4;
+                return -5;
             }
             $investorId = self::fromPaymentServerUserIdToInvestorId($message['userid']);
             Wallet::registerWallet($investorId, $message['coin'], $message['address']);
@@ -108,19 +112,22 @@ class PaymentServer_controller
             //"{"amount": "2.0", "coin": "DOGE", "conf": 1, "keyid": "22c336448554e86b", "nonce": 1511352641577, "reason": "deposit", "txid": "cf3344b4fd3d7cf08fbd3fc19a751a0cf7ec1d776e65bcf3c573bac5c6484f5d", "userid": 666, "vout": 0}"
             $coin = strtoupper($message['coin']);
             if (!Coin::issetCoin($coin)) {
-                return -5;
+                return -6;
             }
             if (!Coin::checkDepositConfirmation($coin, $message['conf'])) {
-                return -6;
+                return -7;
             }
             $investorId = self::fromPaymentServerUserIdToInvestorId($message['userid']);
             if (!Deposit::receiveDeposit((double)$message['amount'], $coin, $message['txid'], $message['vout'], $investorId)) {
-                return -7;
+                return -8;
             }
             return 2;
+        } else if ($message['reason'] === self::NOTIFY_REASON_ALARM) {
+            //"{"data": {"address": "0xf59faa185482b63561858a24cb38b7b0367b103e", "coin": "ETH", "current-balance": 0.0, "minimum-balance": 0.5}, "keyid": "7551b0406fc7bfb2", "message": "Low deposit master account balance", "nonce": 1513260420631, "reason": "alarm"}"
+            return 3;
         }
 
-        return -8;
+        return -9;
     }
 
     /**
