5ab2ca81f2e9a49df491d0c7dc39b8871e2b6d8f
Alexey Rytikov <user.raa@gmail.com>
2017-12-29 12:25:56 +0300

    add more data to "bounty_info"

--- web_root/index.php
+++ web_root/index.php
@@ -11,11 +11,13 @@ Application::init();
 Router::register(function () {
     $bountyInfo = @DB::get("
         SELECT
-            ( SELECT count( * ) FROM investors WHERE eth_bounty = 0 AND eth_new_bounty > 0 ) AS realized_bounty,
-            ( SELECT count( * ) FROM investors WHERE eth_new_bounty > 0 ) AS with_bounty
+            ( SELECT count( * ) FROM investors WHERE eth_bounty = 0 AND eth_new_bounty > 0 ) AS investors_realized_bounty,
+            ( SELECT count( * ) FROM investors WHERE eth_new_bounty > 0 ) AS investors_with_bounty,
+            ( SELECT sum( eth_bounty ) FROM investors ) AS remaining_bonus
     ")[0];
-    echo "with_bounty: {$bountyInfo['with_bounty']}<br>";
-    echo "realized_bounty {$bountyInfo['realized_bounty']}";
+    echo "investors realized bounty {$bountyInfo['investors_realized_bounty']}<br>";
+    echo "investors with bounty: {$bountyInfo['investors_with_bounty']}<br>";
+    echo "remaining bonus {$bountyInfo['remaining_bonus']}<br>";
 }, 'bounty_info');
 
 call_user_func(Router::current());
