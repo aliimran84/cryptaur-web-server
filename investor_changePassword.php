35477b411422bded65d644cc9be8b288a181604a
Alexey Rytikov <user.raa@gmail.com>
2018-01-04 10:58:16 +0300

    add script for changing investor password

---
+++ investor_changePassword.php
@@ -0,0 +1,17 @@
+<?php
+
+require __DIR__ . '/loader.php';
+
+use core\engine\Application;
+use core\models\Investor;
+
+Application::init();
+
+$investor = Investor::getByEmail(@$argv[1]);
+if (!$investor) {
+    echo 'Investor not found';
+} else {
+    $investor->changePassword(@$argv[2]);
+    echo 'Password change. New password length: ' . strlen(@$argv[2]);
+}
+echo "\r\n";
