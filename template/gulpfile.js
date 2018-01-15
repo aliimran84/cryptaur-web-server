e27c6f22315c3f0f6a872402d299a03aa82734d5
KrKrainov <k.a.krainov@gmail.com>
2018-01-15 14:55:16 +0300

    added ether wallet page

--- template/gulpfile.js
+++ template/gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp'),
     concatCss = require('gulp-concat-css'),
     rename = require('gulp-rename'),
-    rev_append = require('gulp-rev-append'),
     minifyCSS = require('gulp-minify-css');
 
 gulp.task('style:build', function() {
@@ -12,11 +11,7 @@ gulp.task('style:build', function() {
         .pipe(gulp.dest('styles/'));
 });
 
-gulp.task('default', ['style:build'], function() {
-    gulp.src(['./reg.html', './login.html', './settings.html', './transactions.html', './dashboard.html', './wallet.html'])
-        .pipe(rev_append())
-        .pipe(gulp.dest('.'));
-});
+gulp.task('default', ['style:build']);
 
 gulp.task('watch', function () {
     gulp.watch(['styles/*.css'],['default']);
