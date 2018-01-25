05feb3646abb9c88113884b63aa15fec72ff45ed
Alexey Rytikov <user.raa@gmail.com>
2018-01-25 12:18:23 +0300

    total_info.json: add total_users

--- setIcoInfo.php
+++ setIcoInfo.php
@@ -10,8 +10,10 @@ Application::init();
 
 $eth = Utility::int_string(DB::get("SELECT SUM(`balance`) AS `sum` FROM `wallets` WHERE `coin`='eth';")[0]['sum']);
 $btc = Utility::int_string(DB::get("SELECT SUM(`balance`) AS `sum` FROM `wallets` WHERE `coin`='btc';")[0]['sum']);
+$users_count = Utility::int_string(DB::get("SELECT COUNT(*) AS `count` FROM `investors`;")[0]['count']);
 
 file_put_contents(\core\controllers\Base_controller::ICOINFO_FILE, json_encode([
     'total_eth' => $eth,
-    'total_btc' => $btc
+    'total_btc' => $btc,
+    'total_users' => $users_count
 ], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE));
