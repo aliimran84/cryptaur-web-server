7a8d1a065e7967b60d9cfec9e8f4b4aa46e3f3d8
Alexey Rytikov <user.raa@gmail.com>
2018-02-04 23:58:57 +0300

    fix_wrongReferrersStructure: add updating db simple investor_id values. Remain concatenated strings.

--- fix_wrongReferrersStructure.php
+++ fix_wrongReferrersStructure.php
@@ -18,17 +18,8 @@ do {
     foreach ($investors_data as $i => $investor_data) {
         ++$lastId;
         // todo: update several tables
-        // eth_queue.investor_id
-        // eth_queue_wallets.investor_id
-        // investors_2fa_choice.investor_id
-        // investors_ethaddresses.investor_id
         // investors_referrals.referrals concatenated str
-        // investors_referrals_tokens.investor_id
-        // investors_referrals_totals.investor_id
-        // investors_referrers.investor_id
         // investors_referrers.referrers concatenated str
-        // investors_stage_0_bounty.investor_id
-        // investors_waiting_tokens.investor_id
         // example for concatenated form
         DB::get("
             SELECT *, TRIM(BOTH ',' FROM REPLACE(concat(',', `referrals`, ','), ',{$investor_data['id']},', ',$lastId,'))
@@ -36,10 +27,19 @@ do {
             where concat(',', `referrals`, ',') like '%,{$investor_data['id']},%';
         ;");
         DB::set("UPDATE `investors` SET `id` = '$lastId' WHERE `id` = {$investor_data['id']} LIMIT 1;");
+        DB::set("UPDATE `investors` SET `referrer_id` = '$lastId' WHERE `referrer_id` = {$investor_data['id']};");
         DB::set("UPDATE `investors_to_previous_system` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']} LIMIT 1;");
         DB::set("UPDATE `investors_referrals_compressed` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
         DB::set("UPDATE `deposits` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
         DB::set("UPDATE `wallets` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `eth_queue` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `eth_queue_wallets` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `investors_2fa_choice` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `investors_ethaddresses` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `investors_referrals_totals` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `investors_referrers` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `investors_stage_0_bounty` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
+        DB::set("UPDATE `investors_waiting_tokens` SET `investor_id` = '$lastId' WHERE `investor_id` = {$investor_data['id']};");
         $duration = (time() - $startTime) + 1;
         $currentCount = $i;
         echo date('Y-m-d H:i:s') . ": fix for $i/$count (duration: {$duration}s)\r\n";
